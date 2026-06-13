a8dc58d8e882c61f863114c4f3f4cf267ec95510
Aman Kharwar <aman.kharwar@difm.tech>
2026-06-13 11:33:42 +0530

    test2

--- src/config/face-api.ts
+++ src/config/face-api.ts
@@ -11,8 +11,8 @@ faceapi.env.monkeyPatch({
 })
 
 export async function loadDataModels() {
-    await faceapi.tf.setBackend('wasm');
-    await faceapi.tf.ready();
+await (faceapi.tf as any).setBackend('wasm');
+    await (faceapi.tf as any).ready();
     console.log("Initialized tfjs-backend-wasm successfully");
 
     const MODEL_URL =
